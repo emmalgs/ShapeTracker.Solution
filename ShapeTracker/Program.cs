714aa0b39bfc86a12440aab21bc1acd1141bfe92
Emma Gerigscott <elgerig@gmail.com>
2023-04-28 16:23:35 -0700

    add constructor method to Triangle class to create a triangle using fields

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -7,8 +7,11 @@ namespace ShapeTracker
   {
     static void Main()
     {
-      Triangle testTriangle = new Triangle();
+      Triangle testTriangle = new Triangle(3,4,5);
       Console.WriteLine(testTriangle.GetType());
+      Console.WriteLine($"Side one of the triangle: {testTriangle.Side1}");
+      Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
+      Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
     }
   }
 }
