02491155978a634b0a50b1f24c5c79ccdaf76d87
Emma Gerigscott <elgerig@gmail.com>
2023-04-28 16:13:47 -0700

    use namespaces for Models and Program, update Program to using ShapeTracker.Models namespace

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -1,10 +1,14 @@
 using System;
+using ShapeTracker.Models;
 
-class Program
+namespace ShapeTracker
 {
-  static void Main()
+  class Program
   {
-    Triangle testTriangle = new Triangle();
-    Console.WriteLine(testTriangle.GetType());
+    static void Main()
+    {
+      Triangle testTriangle = new Triangle();
+      Console.WriteLine(testTriangle.GetType());
+    }
   }
 }
