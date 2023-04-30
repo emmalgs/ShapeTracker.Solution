81812a0aa3696963c4209bd6d6eef498ebc91a1a
Emma Gerigscott <elgerig@gmail.com>
2023-04-30 09:08:07 -0700

    add static method to check if user input is correct

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -20,6 +20,35 @@ namespace ShapeTracker
       int length2 = int.Parse(stringNumber2);
       int length3 = int.Parse(stringNumber3);
       Triangle tri = new Triangle(length1, length2, length3);
+      ConfirmOrEditTriangle(tri);
+    }
+
+    static void ConfirmOrEditTriangle(Triangle tri)
+    {
+      Console.WriteLine("Please confirm that you entered your triangle correctly:");
+      Console.WriteLine($"Side 1 has a length of {tri.GetSide1()}.");
+      Console.WriteLine($"Side 2 has a length of {tri.GetSide2()}.");
+      Console.WriteLine($"Side 3 has a length of {tri.GetSide3()}.");
+      Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter the triangle's sides");
+      string userInput = Console.ReadLine();
+      if (userInput == "yes")
+      {
+        CheckTriangleType(tri);
+      }
+      else
+      {
+        Console.WriteLine("Let's fix your triangle. Please enter the 3 sides again!");
+        Console.WriteLine("Please enter a number:");
+        string stringNumber1 = Console.ReadLine();  
+        Console.WriteLine("Enter another number:");
+        string stringNumber2 = Console.ReadLine();  
+        Console.WriteLine("Enter a third number:");
+        string stringNumber3 = Console.ReadLine();  
+        tri.SetSide1(int.Parse(stringNumber1));  
+        tri.SetSide2(int.Parse(stringNumber2));  
+        tri.SetSide3(int.Parse(stringNumber3)); 
+        ConfirmOrEditTriangle(tri);
+      }
     }
   }
 }
